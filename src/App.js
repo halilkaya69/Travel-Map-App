38619bae1c56bde4551da1586e5b4d715cfe916c
halilkaya <120128959+halilkaya69@users.noreply.github.com>
2024-07-14 14:37:02 +0300

    ı used react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import React, { useState } from 'react';
+import ReactMapGL from "react-map-gl";
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4
+  });
+
   return (
     <div className="App">
-      hello
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
     </div>
   );
 }

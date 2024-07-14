f4f5657e469813e8694d5a95d40d613b8226c983
halilkaya <120128959+halilkaya69@users.noreply.github.com>
2024-07-14 15:16:34 +0300

    Marker added from react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import ReactMapGL from "react-map-gl";
-
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 function App() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
     latitude: 46,
     longitude: 17,
-    zoom: 4
+    zoom: 4,
   });
 
   return (
@@ -15,9 +15,13 @@ function App() {
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-        onViewportChange={nextViewport => setViewport(nextViewport)}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+      >
+        <Marker longitude={2.294694} latitude={48.858093} offsetLeft={-20} offsetTop={-10}>
+          <LocationOnIcon style={{fontSize:viewport.zoom*7,color:"slateblue"}}/>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
